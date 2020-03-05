e031a14212db4e872f3ca807ef1f617c013950f0
Gustavo nRm <gustavonrm@gmail.com>
2020-03-05 16:28:45 +0000

    social networki icons

--- html/templates/tpl_common.php
+++ html/templates/tpl_common.php
@@ -12,6 +12,9 @@ function draw_header(){ ?>
             <!-- bootstrap -->
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
             <link href="../fonts/fontawesome-free-5.12.1-web/css/all.css" rel="stylesheet"> <!--load all styles -->
+            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            
             <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -32,7 +35,7 @@ function draw_navbar(){ ?>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Home &#xf39e;<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
@@ -89,50 +92,36 @@ function draw_footer(){?>
                     </div>
                     <div class="col-sm-2"> 
                         <div class="container-expanded">
-                            <div class="row py-3">
+                            <div class="row py-2">
 
                             </div>
                             <div class="row ">
-                                <div class="col-sm-1">
-
-                                </div>
                                 <div class="col-sm-2">
                                     <a href="../pages/home.php">
-                                        <svg class="bd-placeholder-img rounded-circle" width="50" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 50x50">
-                                            <title>Placeholder</title>
-                                            <rect width="100%" height="100%" fill="#777"></rect>
-                                            <text class="fa" x="50%" y="50%" fill="#777" dy=".3em">&#xf099;</text>
-                                        </svg>
+                                        <i class="fab fa-facebook-f fa-2x circle-icon"></i>
                                     </a>
-                                    
                                 </div>
                                 <div class="col-sm-1"></div>
                                 <div class="col-sm-2">
-                                    <svg class="bd-placeholder-img rounded-circle" width="50" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 50x50">
-                                        <title>Placeholder</title>
-                                        <rect width="100%" height="100%" fill="#777"></rect>
-                                        <g><text x="0" y="0">&#xf099;</text></g>
-                                    </svg>
+                                    <a href="../pages/home.php">
+                                    <i class="fab fa-instagram fa-2x circle-icon"></i>
+                                    </a>
                                 </div>
                                 <div class="col-sm-1"></div>
                                 <div class="col-sm-2">
-                                    <svg class="bd-placeholder-img rounded-circle" width="50" height="50" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 60x60">
-                                        <title>Placeholder</title>
-                                        <rect width="100%" height="100%" fill="#777"></rect>
-                                        <text class="fa" x="50%" y="50%" fill="#777" dy=".3em">&#xf099;</text>
-                                        
-                                    </svg>
+                                    <a href="../pages/home.php">
+                                        <i class="fa fa-twitter fa-2x circle-icon"> </i>
+                                    </a>
                                 </div>
                                 <div class="col-sm-2">
-
+                                Subscribe newsletter
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-2" >
-                    <i class="fab fa-twitter"></i>
+
                     </div>
-              
                 </div>
                 <p class="mx-auto" style="width: 300px;"> Copyright © 2019 LTW FEUP</p> 
             </div>
