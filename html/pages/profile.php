4f5db376790c917d32c1b02c2415b24cb92ef55a
Diogo Machado <diogo.smac@gmail.com>
2020-03-08 01:26:10 +0000

    Basic structure and some formatting of profile page

--- html/pages/profile.php
+++ html/pages/profile.php
@@ -1,9 +1,9 @@
 <?php
 
-    include_once("../templates/tpl_common.php");
-    include_once("../templates/profile_page.php");
+include_once("../templates/tpl_common.php");
+include_once("../templates/profile_page.php");
 
-    draw_header();
+draw_header();
 ?>
 
 <main>
@@ -11,59 +11,69 @@
         <div class="my-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-auto nav flex-column nav-pills" id="v-pills-tab" role="tablist"
-                        aria-orientation="vertical">
-                        <a class="nav-link disabled">John Doe</a>
-                        <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile"
-                            role="tab" aria-controls="v-pills-profile" aria-selected="true">
-                            <i class="material-icons" >person</i>&nbsp;Profile
+
+                    <!-- TABS -->
+                    <div class="col-md-auto nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+
+                        <a class="nav-link disabled" id="profile-user-name">John Doe</a>
+
+                        <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="true">
+                            <i class="material-icons">person</i>&nbsp;Profile
                         </a>
-                        <a class="nav-link" id="v-pills-reviews-tab" data-toggle="pill" href="#v-pills-reviews"
-                            role="tab" aria-controls="v-pills-reviews" aria-selected="false">
-                            <i class="material-icons" >menu_book</i>&nbsp;Reviews
+
+                        <a class="nav-link" id="v-pills-reviews-tab" data-toggle="pill" href="#v-pills-reviews" role="tab" aria-controls="v-pills-reviews" aria-selected="false">
+                            <i class="material-icons">menu_book</i>&nbsp;Reviews
                         </a>
-                        <a class="nav-link" id="v-pills-wishlist-tab" data-toggle="pill" href="#v-pills-wishlist"
-                            role="tab" aria-controls="v-pills-wishlist" aria-selected="false">
-                            <i class="material-icons" >favorite</i>&nbsp;Wishlist
+
+                        <a class="nav-link" id="v-pills-wishlist-tab" data-toggle="pill" href="#v-pills-wishlist" role="tab" aria-controls="v-pills-wishlist" aria-selected="false">
+                            <i class="material-icons">favorite</i>&nbsp;Wishlist
                         </a>
-                        <a class="nav-link" id="v-pills-history-tab" data-toggle="pill" href="#v-pills-history"
-                            role="tab" aria-controls="v-pills-history" aria-selected="false">
-                            <i class="material-icons" >shopping_cart</i>&nbsp;History
+
+                        <a class="nav-link" id="v-pills-history-tab" data-toggle="pill" href="#v-pills-history" role="tab" aria-controls="v-pills-history" aria-selected="false">
+                            <i class="material-icons">shopping_cart</i>&nbsp;History
                         </a>
+
                     </div>
+
+                    <!-- CONTENT -->
                     <div class="col tab-content" id="v-pills-tabContent">
-                        <div class="tab-pane fade show active" id="v-pills-profile" role="tabpanel"
-                            aria-labelledby="v-pills-profile-tab">
+
+                        <div class="tab-pane fade show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
                             <section id="profile">
-                                Mas que profile, este profile!
+                                <?php draw_profile_info(); ?>
                             </section>
                         </div>
-                        <div class="tab-pane fade" id="v-pills-reviews" role="tabpanel"
-                            aria-labelledby="v-pills-reviews-tab">
+
+                        <div class="tab-pane fade" id="v-pills-reviews" role="tabpanel" aria-labelledby="v-pills-reviews-tab">
                             <section id="reviews">
                                 <?php draw_product_review(); ?>
-                                <hr/>
+                                <hr />
                                 <?php draw_product_review(); ?>
-                                <hr/>
+                                <hr />
                                 <?php draw_product_review(); ?>
                             </section>
                         </div>
-                        <div class="tab-pane fade" id="v-pills-wishlist" role="tabpanel"
-                            aria-labelledby="v-pills-wishlist-tab">
+
+                        <div class="tab-pane fade" id="v-pills-wishlist" role="tabpanel" aria-labelledby="v-pills-wishlist-tab">
                             <section id="wishlist">
                                 <?php draw_wishlist_product(); ?>
-                                <hr/>
+                                <hr />
                                 <?php draw_wishlist_product(); ?>
-                                <hr/>
+                                <hr />
                                 <?php draw_wishlist_product(); ?>
                             </section>
                         </div>
-                        <div class="tab-pane fade" id="v-pills-history" role="tabpanel"
-                            aria-labelledby="v-pills-history-tab">
+
+                        <div class="tab-pane fade" id="v-pills-history" role="tabpanel" aria-labelledby="v-pills-history-tab">
                             <section id="history">
-                                Mas que history, este history!
+                                <?php draw_history_item(); ?>
+                                <hr />
+                                <?php draw_history_item(); ?>
+                                <hr />
+                                <?php draw_history_item(); ?>
                             </section>
                         </div>
+
                     </div>
                 </div>
             </div>
@@ -73,6 +83,6 @@
 
 <?php
 
-    draw_footer();
+draw_footer();
 
 ?>
--- html/templates/profile_page.php
+++ html/templates/profile_page.php
@@ -1,101 +1,204 @@
-<?php
+<?php function draw_profile_info()
+{ ?>
 
-function draw_wishlist_product() { ?>
-
-<section id="wishlist-product" class="my-4">
-    <div class="row">
-        <div class="col-md-4">
-            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1"
-                alt="Ink" class="img-fluid">
-        </div>
-        <div class="col" id="wishlist-product-info">
-            <div class="row my-1">
-                <div class="col">
-                    <h2>Black Ink&trade;</h2>
-                </div>
-                <div class="col-md-4">
-                    <button type="button" class="btn float-right btn-outline-danger"><i class="material-icons"
-                            style="color: red;">favorite</i></button>
-                </div>
+    <section id="profile-info">
+        <div class="col">
+            <div class="row" id="profile-tag">
+                <span>Photo</span>
             </div>
-            <div class="row my-1 align-items-center">
-                <div class="col ml-3">
-                    <div class="row">
-                        <i class="material-icons" style="color: gold;">star</i>
-                        <i class="material-icons" style="color: gold;">star</i>
-                        <i class="material-icons" style="color: gold;">star</i>
-                        <i class="material-icons" style="color: gold;">star_half</i>
-                        <i class="material-icons" style="color: gold;">star_outline</i>
-                    </div>
-                    <div class="row align-items-center">
-                        <h4>Available&nbsp;</h4>
-                        <i class="material-icons" style="color: green;">fiber_manual_record</i>
-                    </div>
-                </div>
-                <div class="col">
-                    <h3 class="align-middle float-right">17.99€</h3>
-                </div>
-            </div>
-            <div class="row my-1">
-                <div class="col ml-3">
-                    <div class="row">
-                        <label class="input-group-text" for="inputGroupSelect01">QTY</label>
-                        <input type="number" value="1" min="1" max="30" step="1" />
-                    </div>
-                </div>
-                <div class="col">
-                    <input id="add-to-cart-btn" class="btn float-right btn-primary" type="submit" value="ADD TO CART">
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<?php }
-
-
-function draw_product_review() {
-?>
-
-<section id="product-review" class="my-4">
-    <div class="row">
-        <div class="col-md-4">
-            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1"
-                alt="Ink" class="img-fluid">
-        </div>
-        <div class="col" id="product-review-info">
-            <div class="row my-1 align-items-center">
-                <div class="col">
-                    <h2>Black Ink&trade;</h2>
-                </div>
-                <div class="col-md-4">
-                    <button type="button" class="btn float-right"><i class="material-icons">edit</i></button>
-                </div>
-            </div>
-            <div class="row my-1">
+            <div class="row align-items-center" id="profile-photo">
                 <div class="col-md-auto">
-                    <h5>Cor viva, pouco resistente.</h5>
+                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
                 </div>
-                <div class="col-md-4">
-                    <div class="row float-right">
-                        <i class="material-icons" style="color: gold;">star</i>
-                        <i class="material-icons" style="color: gold;">star</i>
-                        <i class="material-icons" style="color: gold;">star</i>
-                        <i class="material-icons" style="color: gold;">star_half</i>
-                        <i class="material-icons" style="color: gold;">star_outline</i>
+                <div class="col">
+                    <button class="btn btn-primary">Edit your profile</button>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row" id="profile-tag">
+            <span>General</span>
+        </div>
+        <div class="row" id="profile-name">
+            <span>John Doe</span>
+        </div>
+        <div class="row" id="profile-dob">
+            <span>25-04-1974</span>
+        </div>
+
+        <hr>
+
+        <div class="row" id="profile-tag">
+            <span>Contact</span>
+        </div>
+        <div class="row" id="profile-email">
+            <span>john@doe.co.uk</span>
+        </div>
+        <div class="row" id="profile-phone">
+            <span>+351 912345678</span>
+        </div>
+
+        <hr>
+
+        <div class="row" id="profile-tag">
+            <span>Billing</span>
+        </div>
+        <div class="row" id="profile-card">
+            <span>MasterCard ending in XXXX</span>
+        </div>
+        <div class="row" id="profile-address">
+            <span>Address St., 123 - 6º Esq. Frente</span>
+        </div>
+    </section>
+
+<?php } ?>
+
+
+<?php function draw_product_review()
+{ ?>
+
+    <section id="product-review">
+        <div class="row" id="review-container">
+            <div class="col-md-4">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
+            </div>
+            <div class="col" id="product-review-info">
+                <div class="row my-1 align-items-center">
+                    <div class="col" id="review-text">
+                        <span class="review-product-name float-left">Black Ink</span>
+                    </div>
+                    <div class="col-md-auto">
+                        <button type="button" class="btn float-right" id="edit-review-button"><i class="material-icons">edit</i></button>
+                    </div>
+                </div>
+                <div class="row my-1 align-items-center">
+                    <div class="col" id="review-text">
+                        <span class="review-title">Cor viva, pouco resistente.</span>
+                        <!-- <span class="review-title">Cor viva, pouco resistente.</span> -->
+                    </div>
+                    <div class="col-md-auto">
+                        <div class="row float-right">
+                            <i class="material-icons" style="color: gold;">star</i>
+                            <i class="material-icons" style="color: gold;">star</i>
+                            <i class="material-icons" style="color: gold;">star</i>
+                            <i class="material-icons" style="color: gold;">star_half</i>
+                            <i class="material-icons" style="color: gold;">star_outline</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="row my-1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php } ?>
+
+
+<?php function draw_wishlist_product()
+{ ?>
+
+    <section id="wishlist-product">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
+            </div>
+            <div class="col" id="wishlist-product-info">
+                <div class="row my-1">
+                    <div class="col">
+                        <h2>Black Ink</h2>
+                    </div>
+                    <div class="col-md-4">
+                        <button type="button" class="btn float-right btn-outline-danger"><i class="material-icons" style="color: red;">favorite</i></button>
+                    </div>
+                </div>
+                <div class="row my-1 align-items-center">
+                    <div class="col ml-3">
+                        <div class="row">
+                            <i class="material-icons" style="color: gold;">star</i>
+                            <i class="material-icons" style="color: gold;">star</i>
+                            <i class="material-icons" style="color: gold;">star</i>
+                            <i class="material-icons" style="color: gold;">star_half</i>
+                            <i class="material-icons" style="color: gold;">star_outline</i>
+                        </div>
+                        <div class="row align-items-center">
+                            <h4>Available&nbsp;</h4>
+                            <i class="material-icons" style="color: green;">fiber_manual_record</i>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <h3 class="align-middle float-right">17.99€</h3>
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col ml-3">
+                        <div class="row">
+                            <label class="input-group-text" for="inputGroupSelect01">QTY</label>
+                            <input type="number" value="1" min="1" max="30" step="1" />
+                        </div>
+                    </div>
+                    <div class="col">
+                        <input id="add-to-cart-btn" class="btn float-right btn-primary" type="submit" value="ADD TO CART">
                     </div>
                 </div>
             </div>
-            <div class="row my-1">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum."
+        </div>
+    </section>
+
+<?php } ?>
+
+
+<?php function draw_purchase_item()
+{ ?>
+
+    <div class="row align-items-center">
+        <div class="col-md-5">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
+        </div>
+        <div class="col">
+            <div class="row">
+                <span>Product Name ABC</span>
+            </div>
+            <div class="row">
+                <span>13.99€</span>
             </div>
         </div>
     </div>
-</section>
 
-<?php }
-?>
+<?php } ?>
+
+<?php function draw_history_item()
+{ ?>
+
+    <section id="history-purchase">
+        <div class="col" id="review-container">
+            <div class="row" id="purchase-address">
+                <span>Address St., 123 - 6º Esq. Frente</span>
+            </div>
+            <div class="row" id="purchase-details">
+                <span>27/02/2020</span>
+                <span>&nbsp;-&nbsp;</span>
+                <span>Shipped</span>
+            </div>
+            <div class="col my-3" id="purchase-items-container">
+                <?php draw_purchase_item() ?>
+                <hr>
+                <?php draw_purchase_item() ?>
+            </div>
+            <div class="row" id="purchase-shipping">
+                <span>2,02€</span>
+            </div>
+            <div class="row" id="purchase-total">
+                <span>30,00€</span>
+            </div>
+        </div>
+    </section>
+
+<?php } ?>
