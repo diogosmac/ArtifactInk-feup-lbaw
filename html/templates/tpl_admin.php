3da0d7f0b65f8ab93a1d405defd1b386bf368fd7
Marantesss <up201706917@fe.up.pt>
2020-03-08 12:13:06 +0000

    Finished Home Overview cards

--- html/templates/tpl_admin.php
+++ html/templates/tpl_admin.php
@@ -34,7 +34,7 @@ function draw_navbar()
       <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
         <div class="collapse navbar-collapse my-2">
 
-          <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Artifact Ink</a>
+          <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Artifact Ink Admin</a>
 
           <ul class="navbar-nav ml-auto mr-2">
             <li class="nav-item">
@@ -61,87 +61,249 @@ function draw_navbar()
 
   <?php }
 
+function draw_home()
+{ ?>
+    <div class="container-fluid">
+      <div class="row">
+
+        <?php draw_sidebar() ?>
+
+        <main class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <div class="border-bottom mt-2">
+            <h1>Overview</h1>
+          </div>
+
+          <div class="container row mx-auto my-2">
+
+            <div class="col-lg-3 col-md-6 col-sm-6 py-2">
+              <div class="card text-center">
+                <div class="card-body">
+                  <h5 class="card-title">Users</h5>
+                  <table class="table table-sm mb-0">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Users Registered</th>
+                        <td>675</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Users Online</th>
+                        <td>12</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Site Visitors</th>
+                        <td>8694</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 py-2">
+              <div class="card text-center">
+                <div class="card-body">
+                  <h5 class="card-title">Products</h5>
+                  <table class="table table-sm mb-0">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Total Products</th>
+                        <td>98</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Products On Sale</th>
+                        <td>14</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Product Views</th>
+                        <td>8694</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 py-2">
+              <div class="card text-center">
+                <div class="card-body">
+                  <h5 class="card-title">Orders</h5>
+                  <table class="table table-sm mb-0">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Total Orders</th>
+                        <td>562</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Orders Shipped</th>
+                        <td>496</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Orders Received</th>
+                        <td>365</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 py-2">
+              <div class="card text-center">
+                <div class="card-body">
+                  <h5 class="card-title">Reviews</h5>
+                  <table class="table table-sm mb-0">
+                    <tbody>
+                      <tr>
+                        <th scope="row">Total Orders</th>
+                        <td>562</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Orders Shipped</th>
+                        <td>496</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Orders Received</th>
+                        <td>365</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="border-bottom mt-2">
+            <h1>Notifications</h1>
+          </div>
+
+          <div class="container row mx-auto my-2">
+          </div>
+
+        </main>
+      </div>
+    </div>
+
+  <?php }
+
 /**
  * Function to draw admin area top navbar
  */
 function draw_sidebar()
 { ?>
-    <div class="container-fluid">
-      <div class="row">
-        <!-- side bar with options -->
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
+    <!-- side bar with options -->
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky d-flex flex-column">
 
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Admin Dashboard</h6>
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <i>ICON</i>
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Categories
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Orders
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Reviews
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Users
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Sales
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Make Newsletter
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  FAQ
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <i>ICON</i>
-                  Info
-                </a>
-              </li>
-            </ul>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Admin Dashboard</h6>
 
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+              </svg>
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag">
+                <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <path d="M16 10a4 4 0 0 1-8 0"></path>
+              </svg>
+              Products
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard">
+                <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+                <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+              </svg>
+              Categories
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-package">
+                <line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line>
+                <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
+                <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+                <line x1="12" y1="22.08" x2="12" y2="12"></line>
+              </svg>
+              Orders
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2">
+                <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+              </svg>
+              Reviews
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
+                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                <circle cx="9" cy="7" r="4"></circle>
+                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+              </svg>
+              Users
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag">
+                <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path>
+                <line x1="7" y1="7" x2="7.01" y2="7"></line>
+              </svg>
+              Sales
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-send">
+                <line x1="22" y1="2" x2="11" y2="13"></line>
+                <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+              </svg>
+              Make Newsletter
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle">
+                <circle cx="12" cy="12" r="10"></circle>
+                <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                <line x1="12" y1="17" x2="12.01" y2="17"></line>
+              </svg>
+              FAQ
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info">
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="12" y1="16" x2="12" y2="12"></line>
+                <line x1="12" y1="8" x2="12.01" y2="8"></line>
+              </svg>
+              Info
+            </a>
+          </li>
+        </ul>
 
-          </div>
-        </nav>
+        <div class="mt-auto mb-3">
+          <a class="nav-link" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
+              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+              <polyline points="16 17 21 12 16 7"></polyline>
+              <line x1="21" y1="12" x2="9" y2="12"></line>
+            </svg>
+            Sign Out
+          </a>
+        </div>
       </div>
-    </div>
-
+    </nav>
   <?php }
 
 /**
