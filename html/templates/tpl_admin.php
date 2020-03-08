260c1fb78c517d250349333af9dc23c4b2ced0df
Marantesss <up201706917@fe.up.pt>
2020-03-08 12:52:08 +0000

    removed unecessary classes from sidebar h6 title

--- html/templates/tpl_admin.php
+++ html/templates/tpl_admin.php
@@ -234,7 +234,7 @@ function draw_sidebar()
     <nav class="col-md-2 d-none d-md-block bg-light sidebar">
       <div class="sidebar-sticky d-flex flex-column">
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Admin Dashboard</h6>
+        <h6 class="sidebar-heading px-3 mt-4 mb-1 text-muted">Admin Dashboard</h6>
 
         <ul class="nav flex-column">
           <li class="nav-item">
