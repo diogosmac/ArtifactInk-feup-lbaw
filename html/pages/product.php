98415854b73531a16ec205547356ba4e9c2b04c5
Tiago Silva <tiago.silva.99@hotmail.com>
2020-03-09 15:47:13 +0000

    Desktop Insert Review Popup

--- html/pages/product.php
+++ html/pages/product.php
@@ -28,6 +28,7 @@ draw_navbar(true);
         <?php draw_card_carousel('featured'); ?>
     </div>
     <hr class="w-75">
+
     <section id="specs" class="mx-auto">
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
@@ -78,29 +79,6 @@ draw_navbar(true);
             </div>
         </div>
     </section>
-    <div class="d-flex flex-column border rounded border-secondary bg-light w-25 mx-auto">
-        <div class="px-2">
-            <button type="button" class="close" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="d-flex flex-column align-items-center justify-content-around px-4 pb-3">
-            <h4>Black Ink</h4>
-            <div class="d-flex flex-row justify-content-start mt-2 w-100">
-                <h5>Rating</h5>
-                <div class="d-flex flex-row px-3">
-                    <i class="material-icons" style="color: gold;">star_outline</i>
-                    <i class="material-icons" style="color: gold;">star_outline</i>
-                    <i class="material-icons" style="color: gold;">star_outline</i>
-                    <i class="material-icons" style="color: gold;">star_outline</i>
-                    <i class="material-icons" style="color: gold;">star_outline</i>
-                </div>
-            </div>
-            <input class="form-control m-1" type="text" placeholder="Subject">
-            <textarea class="form-control m-1" rows="3" placeholder="Tell us what you think"></textarea>
-            <button type="button" class="btn btn-primary w-50 mt-2">Submit</button>
-        </div>
-    </div>
 </main>
 
 <?php
