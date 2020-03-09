6ad13fd08ff5967cbc00a2c3e53c7cea38fbcdd7
Diogo Machado <diogo.smac@gmail.com>
2020-03-09 16:13:01 +0000

    review tweaks

--- html/templates/tpl_profile.php
+++ html/templates/tpl_profile.php
@@ -8,7 +8,7 @@
             </div>
             <div class="row align-items-center" id="profile-photo">
                 <div class="col">
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
+                    <img src="https://i1.rgstatic.net/ii/profile.image/731946594365440-1551521075108_Q512/Manuel_Torres42.jpg" alt="Person" class="img-fluid">
                 </div>
                 <div class="col-md-auto" id="profile-edit-button">
                     <button class="btn btn-primary">Edit your profile</button>
@@ -87,11 +87,13 @@
                                         <form>
                                             <div class="form-group">
                                                 <label for="exampleFormControlInput1">Review Title</label>
-                                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Cor viva, pouco resistente.">
+                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="1">Cor viva, pouco resistente.</textarea>
                                             </div>
                                             <div class="form-group">
                                                 <label for="exampleFormControlTextarea1">Review</label>
-                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                                </textarea>
                                             </div>
                                         </form>
                                     </div>
@@ -107,7 +109,6 @@
                 <div class="row my-1 align-items-center">
                     <div class="col" id="review-text">
                         <span class="review-title">Cor viva, pouco resistente.</span>
-                        <!-- <span class="review-title">Cor viva, pouco resistente.</span> -->
                     </div>
                     <div class="col-md-auto" id="profile-review-rating">
                         <div class="row float-right">
@@ -135,23 +136,23 @@
 
 <?php function draw_wishlist_prod_info()
 { ?>
-    <div class="col-md-3">
+    <div class="col-md-4" id="wishlist-product-picture">
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
     </div>
-    <div class="d-flex flex-column col" id="wishlist-product-info">
-        <div class="d-flex flex-row my-1 align-items-center">
-            <div class="col">
+    <div class="d-flex flex-column col justify-content-center" id="wishlist-product-info">
+        <div class="d-flex flex-row my-1 justify-content-between align-items-center">
+            <div class="col-md-auto">
                 <div class="d-flex flex-row">
                     <span id="wishlist-product-name">Black Ink</span>
                 </div>
             </div>
-            <div class="col-md-auto" id="wishlist-product-remove">
+            <div class="col-md-auto float-right" id="wishlist-product-remove">
                 <button type="button" class="btn float-right btn-outline-danger"><i class="material-icons" style="color: red;">favorite</i></button>
             </div>
         </div>
-        <div class="d-flex flex-row my-1">
-            <div class="col">
-                <div class="d-flex flex-row">
+        <div class="d-flex flex-row justify-content-between my-1">
+            <div class="col-md-auto" id="wishlist-product-details">
+                <div class="d-flex flex-row" id="wishlist-product-rating">
                     <i class="material-icons" style="color: gold;">star</i>
                     <i class="material-icons" style="color: gold;">star</i>
                     <i class="material-icons" style="color: gold;">star</i>
@@ -163,18 +164,18 @@
                     <i class="material-icons" style="color: green;">fiber_manual_record</i>
                 </div>
             </div>
-            <div class="col">
-                <span class="align-middle float-right" id="wishlist-product-price">17.99€</span>
+            <div class="col-md-auto" id="wishlist-product-price">
+                <span class="align-middle">17.99€</span>
             </div>
         </div>
-        <div class="d-flex flex-row my-1">
-            <div class="col">
+        <div class="d-flex flex-row justify-content-between my-1">
+            <div class="col-md-auto">
                 <div class="d-flex flex-row">
                     <label class="input-group-text" for="inputGroupSelect01">QTY</label>
                     <input type="number" value="1" min="1" max="30" step="1" />
                 </div>
             </div>
-            <div class="col">
+            <div class="col-md-auto">
                 <input id="add-to-cart-btn" class="btn float-right btn-primary" type="submit" value="ADD TO CART">
             </div>
         </div>
