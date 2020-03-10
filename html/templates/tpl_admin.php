53c1352e7e2f2b30a4ef21ab28df3fb00d2646aa
Marantesss <up201706917@fe.up.pt>
2020-03-10 09:33:25 +0000

    Finished categories page

--- html/templates/tpl_admin.php
+++ html/templates/tpl_admin.php
@@ -315,7 +315,7 @@ function draw_home()
 
         <div class="container">
           <div class="mb-4 border-bottom mt-2">
-            <h1>Notifications</h1>
+            <h1 id="notifications">Notifications</h1>
           </div>
 
           <div class="mx-auto my-2">
@@ -631,6 +631,162 @@ function draw_categories()
 
           <div class="mb-4 border-bottom mt-2">
             <h1>Categories</h1>
+          </div>
+
+          <div class="mx-auto mt-2">
+            <div class="row">
+              <div class="col-md-6 col-sm-12">
+                <div class="d-flex justify-content-between align-items-center flex-wrap mt-2">
+                  <h3>Category</h3>
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addCategory">
+                    Add Category
+                  </button>
+                  <!-- Modal -->
+                  <div class="modal fade" id="addCategory" tabindex="-1" role="dialog" aria-labelledby="question0Modal" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="question0Modal">Add Category</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <form>
+                            <div class="form-group">
+                              <label for="categoryName">Name</label>
+                              <input type="text" class="form-control" id="categoryName" placeholder="Write name here...">
+                            </div>
+                          </form>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Add Category</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="input-group my-3 mr-sm-2">
+                  <input class="form-control" placeholder="Search" aria-label="Search">
+                </div>
+                <table class="table table-striped table-hover text-center">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th class="align-middle" scope="row">3</th>
+                      <td class="align-middle">Ink</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th class="align-middle" scope="row">2</th>
+                      <td class="align-middle">Machines</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th class="align-middle" scope="row">1</th>
+                      <td class="align-middle">Designs</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-md-6 col-sm-12">
+                <div class="d-flex justify-content-between align-items-center flex-wrap mt-2">
+                  <h3>Subcategory</h3>
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addSubcategory">
+                    Add Subcategory
+                  </button>
+                  <!-- Modal -->
+                  <div class="modal fade" id="addSubcategory" tabindex="-1" role="dialog" aria-labelledby="question0Modal" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="question0Modal">Add Subcategory</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <form>
+                            <div class="form-group">
+                              <label for="subcategoryName">Name</label>
+                              <input type="text" class="form-control" id="subcategoryName" placeholder="Write name here...">
+                            </div>
+                          </form>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Add Subcategory</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="input-group my-3 mr-sm-2">
+                  <input class="form-control" placeholder="Search" aria-label="Search">
+                </div>
+                <table class="table table-striped table-hover text-center">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Name</th>
+                      <th scope="col"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th class="align-middle" scope="row">3</th>
+                      <td class="align-middle">Black</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th class="align-middle" scope="row">2</th>
+                      <td class="align-middle">MakePaint Machines</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th class="align-middle" scope="row">1</th>
+                      <td class="align-middle">Dotwork</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th class="align-middle" scope="row">1</th>
+                      <td class="align-middle">Realism</td>
+                      <td class="align-middle">
+                        <button type="button" class="btn btn-secondary mx-2" onclick="location.href='/admin/add_product.php'">Edit</button>
+                        <button type="button" class="btn btn-danger mx-2">Remove</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
 
         </div>
