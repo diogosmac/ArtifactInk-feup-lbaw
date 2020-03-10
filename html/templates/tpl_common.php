ea52ef2ea40a93c9401636279125e1e4e27f9e06
Diogo Machado <diogo.smac@gmail.com>
2020-03-10 02:15:20 +0000

    I AM A GOD. HURRY UP WITH MY DAMN MASSAGE.

--- html/templates/tpl_common.php
+++ html/templates/tpl_common.php
@@ -16,6 +16,7 @@ function draw_header(){ ?>
             <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
         </head>
         <body>
     <?php draw_navbar(); ?>
@@ -26,7 +27,7 @@ function draw_header(){ ?>
  * Function to draw website navbar
  */
 function draw_navbar(){ ?>
-   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" id="navbar">
         <a class="navbar-brand" href="#">Carousel</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- html/templates/tpl_profile.php
+++ html/templates/tpl_profile.php
@@ -2,54 +2,73 @@
 { ?>
 
     <section id="profile-info">
-        <div class="col">
+        <div class="col px-0">
+            <div class="row justify-content-center d-md-none">
+                <button class="btn btn-primary">Edit Profile</button>
+            </div>
+
             <div class="row" id="profile-tag">
                 <span>Photo</span>
             </div>
-            <div class="row align-items-center" id="profile-photo">
-                <div class="col">
-                    <img src="https://i1.rgstatic.net/ii/profile.image/731946594365440-1551521075108_Q512/Manuel_Torres42.jpg" alt="Person" class="img-fluid">
-                </div>
-                <div class="col-md-auto" id="profile-edit-button">
-                    <button class="btn btn-primary">Edit your profile</button>
+
+            <div class="row align-content-center d-md-none">
+                <img src="https://i1.rgstatic.net/ii/profile.image/731946594365440-1551521075108_Q512/Manuel_Torres42.jpg" alt="Person" class="img-fluid">
+            </div>
+    
+            <div class="d-none d-md-block my-4">
+                <div class="row align-items-center">
+                    <div class="col" id="profile-photo">
+                        <img src="https://i1.rgstatic.net/ii/profile.image/731946594365440-1551521075108_Q512/Manuel_Torres42.jpg" alt="Person" class="img-fluid">
+                    </div>
+                    <div class="col-md-auto" id="profile-edit-button">
+                        <button class="btn btn-primary my-2">Edit Profile</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <hr>
-
-        <div class="row" id="profile-tag">
-            <span>General</span>
-        </div>
-        <div class="row" id="profile-name">
-            <span>John Doe</span>
-        </div>
-        <div class="row" id="profile-dob">
-            <span>25-04-1974</span>
-        </div>
-
-        <hr>
-
-        <div class="row" id="profile-tag">
-            <span>Contact</span>
-        </div>
-        <div class="row" id="profile-email">
-            <span>john@doe.co.uk</span>
-        </div>
-        <div class="row" id="profile-phone">
-            <span>+351 912345678</span>
-        </div>
-
-        <hr>
-
-        <div class="row" id="profile-tag">
-            <span>Billing</span>
-        </div>
-        <div class="row" id="profile-card">
-            <span>MasterCard ending in XXXX</span>
-        </div>
-        <div class="row" id="profile-address">
-            <span>Address St., 123 - 6º Esq. Frente</span>
+            <hr>
+    
+            <div class="row" id="profile-tag">
+                <span>General</span>
+            </div>
+            <div class="col my-2" id="profile-name">
+                <span class="row profile-field">Name</span>
+                <span>John Doe</span>
+            </div>
+            <div class="col my-2" id="profile-dob">
+                <span class="row profile-field">Date of Birth</span>
+                <span>25-04-1974</span>
+            </div>
+    
+            <hr>
+    
+            <div class="row" id="profile-tag">
+                <span>Contact</span>
+            </div>
+            <div class="col my-2" id="profile-email">
+                <span class="row profile-field">E-mail</span>
+                <span>john@doe.co.uk</span>
+            </div>
+            <div class="col my-2" id="profile-phone">
+                <span class="row profile-field">Phone Number</span>
+                <span>+351 912345678</span>
+            </div>
+    
+            <hr>
+    
+            <div class="row" id="profile-tag">
+                <span>Billing</span>
+            </div>
+            <div class="col my-2" id="profile-card">
+                <span class="row profile-field">Credit Card</span>
+                <span>MasterCard ending in XXXX</span>
+            </div>
+            <div class="col my-2" id="profile-address">
+                <span class="row profile-field">Address 1</span>
+                <span>Address St., 123 - 6º Esq. Frente</span>
+                <span class="row profile-field">Address 2</span>
+                <span>Avenida de Sérgio Sobral Nunes, 2020</span>
+            </div>
         </div>
     </section>
 
@@ -139,7 +158,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
     <div class="col-md-4" id="wishlist-product-picture">
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
     </div>
-    <div class="d-flex flex-column col justify-content-center" id="wishlist-product-info">
+    <div class="d-flex flex-column col-md-8 justify-content-center" id="wishlist-product-info">
         <div class="d-flex flex-row my-1 justify-content-between align-items-center">
             <div class="col-md-auto">
                 <div class="d-flex flex-row">
@@ -168,17 +187,37 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
                 <span class="align-middle">17.99€</span>
             </div>
         </div>
-        <div class="d-flex flex-row justify-content-between my-1">
-            <div class="col-md-auto">
-                <div class="d-flex flex-row">
-                    <label class="input-group-text" for="inputGroupSelect01">QTY</label>
-                    <input type="number" value="1" min="1" max="30" step="1" />
+    
+        <div class="d-none d-md-block">
+            <div class="d-flex flex-row justify-content-between my-1">
+                <div class="col-md-auto">
+                    <div class="d-flex flex-row align-content-stretch">
+                        <label class="input-group-text" for="inputGroupSelect01">QTY</label>
+                        <input type="number" value="1" min="1" max="30" step="1" />
+                    </div>
+                </div>
+                <div class="col-md-auto">
+                    <button class="btn float-right btn-primary" id="wishlist-add-to-cart">ADD TO CART</button>
                 </div>
             </div>
-            <div class="col-md-auto">
-                <input id="add-to-cart-btn" class="btn float-right btn-primary" type="submit" value="ADD TO CART">
+        </div>
+
+        <div class="d-md-none">
+            <div class="d-flex flex-row justify-content-between my-1">
+                <div class="col-md-auto">
+                    <div class="d-flex flex-row align-content-stretch">
+                        <label class="input-group-text" for="inputGroupSelect01">QTY</label>
+                        <input type="number" value="1" min="1" max="30" step="1" />
+                    </div>
+                </div>
+                <div class="col-md-auto">
+                    <button class="btn float-right btn-primary" id="wishlist-add-to-cart">
+                        <i class="material-icons">add_shopping_cart</i>
+                    </button>
+                </div>
             </div>
         </div>
+
     </div>
 
 <?php } ?>
@@ -199,10 +238,10 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 { ?>
 
     <div class="row align-items-center">
-        <div class="col-md-5">
+        <div class="col-md-3" id="purchase-history-item-picture">
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
         </div>
-        <div class="col">
+        <div class="col my-2" id="purchase-history-item-data">
             <div class="row">
                 <span>Product Name ABC</span>
             </div>
@@ -227,16 +266,92 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
                 <span>&nbsp;-&nbsp;</span>
                 <span>Shipped</span>
             </div>
+
             <div class="col my-3" id="purchase-items-container">
                 <?php draw_purchase_item() ?>
                 <hr>
                 <?php draw_purchase_item() ?>
             </div>
+
             <div class="row" id="purchase-shipping">
                 <span>2,02€</span>
             </div>
             <div class="row" id="purchase-total">
                 <span>30,00€</span>
+            </div>
+    </section>
+
+<?php } ?>
+
+
+
+
+
+
+
+<?php function draw_profile_info_mobile()
+{ ?>
+
+    <section id="profile-info">
+        <div class="mx-5">
+            <div class="col">
+                <div class="row">
+                    <h1 class="display-1"><strong>Photo</strong></h1>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col">
+                        <img src="https://i1.rgstatic.net/ii/profile.image/731946594365440-1551521075108_Q512/Manuel_Torres42.jpg" alt="Person" class="img-fluid w-100 p-3">
+                    </div>
+                    <div class="col-md-auto" id="profile-edit-button">
+                        <button class="btn btn-primary">
+                            <h1 class="display-1">
+                                Edit
+                            </h1>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row" id="profile-tag">
+                <h1 class="display-1"><strong>General</strong></h1>
+            </div>
+            <div id="profile-name">
+                <h1 class="display-2">Name</h1>
+                <h1 class="display-4">John Doe</h1>
+            </div>
+            <div id="profile-dob">
+                <h1 class="display-2">Date of Birth</strong></h1>
+                <h1 class="display-4">25-04-1974</h1>
+            </div>
+
+            <hr>
+
+            <div class="row" id="profile-tag">
+                <h1 class="display-1"><strong>Contact</strong></h1>
+            </div>
+            <div id="profile-email">
+                <h1 class="display-2">E-mail</h1>
+                <h1 class="display-4">john@doe.co.uk</h1>
+            </div>
+            <div id="profile-phone">
+                <h1 class="display-2">Phone Number</h1>
+                <h1 class="display-4">+351 912345678</h1>
+            </div>
+
+            <hr>
+
+            <div class="row" id="profile-tag">
+                <h1 class="display-1"><strong>Billing</strong></h1>
+            </div>
+            <div id="profile-card">
+                <h1 class="display-2">Credit Card</h1>
+                <h1 class="display-4">MasterCard ending in XXXX</h1>
+            </div>
+            <div id="profile-address">
+                <h1 class="display-2">Address</h1>
+                <h1 class="display-4">Address St., 123 - 6º Esq. Frente</h1>
             </div>
         </div>
     </section>
