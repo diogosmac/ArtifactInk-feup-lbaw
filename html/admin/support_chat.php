55d195f374737f18e111469d277c40903476039b
Marantesss <up201706917@fe.up.pt>
2020-03-10 15:21:22 +0000

    Started working on support chat

---
+++ html/admin/support_chat.php
@@ -0,0 +1,128 @@
+<?php
+include_once("../templates/tpl_admin.php");
+
+draw_header();
+draw_navbar();
+?>
+
+<div class="container-fluid">
+  <div class="row">
+
+    <?php draw_sidebar("") ?>
+
+    <main class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div class="container">
+
+        <div class="mb-4 border-bottom mt-2">
+          <h1>Support Messaging</h1>
+        </div>
+
+        <div class="row my-3">
+
+          <!-- Adress Cards -->
+          <div class="col-md-4 px-1">
+            <div class="chat-address">
+
+              <div class="border-bottom card chat-card p-2 m-1">
+                <div class="row align-items-center no-gutters">
+                  <div class="col-md-4">
+                    <img src="https://jet7.pt/wp-content/uploads/2019/10/img_414x371242019_04_09_21_12_15_195560.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Fátima Lopes</h5>
+                      <p class="card-text">Need some help...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="border-bottom card chat-card p-2 m-1">
+                <div class="row align-items-center no-gutters">
+                  <div class="col-md-4">
+                    <img src="https://jet7.pt/wp-content/uploads/2019/10/img_414x371242019_04_09_21_12_15_195560.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Fátima Lopes</h5>
+                      <p class="card-text">Need some help...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="border-bottom card chat-card p-2 m-1">
+                <div class="row align-items-center no-gutters">
+                  <div class="col-md-4">
+                    <img src="https://jet7.pt/wp-content/uploads/2019/10/img_414x371242019_04_09_21_12_15_195560.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Fátima Lopes</h5>
+                      <p class="card-text">Need some help...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="border-bottom card chat-card p-2 m-1">
+                <div class="row align-items-center no-gutters">
+                  <div class="col-md-4">
+                    <img src="https://jet7.pt/wp-content/uploads/2019/10/img_414x371242019_04_09_21_12_15_195560.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Fátima Lopes</h5>
+                      <p class="card-text">Need some help...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="border-bottom card chat-card p-2 m-1">
+                <div class="row align-items-center no-gutters">
+                  <div class="col-md-4">
+                    <img src="https://jet7.pt/wp-content/uploads/2019/10/img_414x371242019_04_09_21_12_15_195560.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Fátima Lopes</h5>
+                      <p class="card-text">Need some help...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="border-bottom card chat-card p-2 m-1">
+                <div class="row align-items-center no-gutters">
+                  <div class="col-md-4">
+                    <img src="https://jet7.pt/wp-content/uploads/2019/10/img_414x371242019_04_09_21_12_15_195560.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Fátima Lopes</h5>
+                      <p class="card-text">Need some help...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-md-8 px-1">
+            <div class="chat-messenger">
+              
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </main>
+  </div>
+</div>
+
+<?php
+draw_footer();
+?>
--- html/templates/tpl_admin.php
+++ html/templates/tpl_admin.php
@@ -40,7 +40,7 @@ function draw_navbar()
 
           <ul class="navbar-nav ml-auto mr-2">
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="/admin/support_chat.php">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                   <polyline points="22,6 12,13 2,6"></polyline>
