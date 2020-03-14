ca740e9bad54f3747216d41f3d7407fc2292a2f0
Tiago Silva <tiago.silva.99@hotmail.com>
2020-03-14 21:09:59 +0000

    hyperlinks

--- html/pages/payments_and_shipment.php
+++ html/pages/payments_and_shipment.php
@@ -7,7 +7,7 @@ draw_navbar(true);
 ?>
 <section class="mx-auto my-3">
     <article class="bg-light p-4">
-        <h1 class="px-2">Payment and Orders</h1>
+        <h1 class="px-2">Payments and Shipment</h1>
         <div class="mt-3">
             <p>
                 Nullam tempor tortor at imperdiet maximus. Nulla pretium viverra leo et ultricies. Donec vel tellus tempus, bibendum enim nec, sollicitudin sem. Pellentesque imperdiet consequat dolor, pharetra lobortis neque. Phasellus iaculis sapien metus, vel gravida urna tincidunt hendrerit. Fusce in porta velit, at rutrum metus. Nunc at leo id massa vehicula convallis. In lacinia dapibus auctor. Pellentesque quam nibh, consequat quis arcu ac, suscipit vehicula nulla. Suspendisse sem urna, laoreet sit amet elit bibendum, condimentum rutrum libero. Praesent in pharetra augue.
