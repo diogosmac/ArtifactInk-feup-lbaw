2839d03a0bfec75e83a6f14dd45c4d0124cb8d5f
Marantesss <up201706917@fe.up.pt>
2020-03-15 23:14:57 +0000

    Finished User search

--- html/admin/users.php
+++ html/admin/users.php
@@ -18,13 +18,42 @@
         </div>
 
         <div class="input-group my-3 mr-sm-2">
-          <input class="form-control" placeholder="Search" aria-label="Search">
+          <input class="form-control" placeholder="Search User" aria-label="Search User">
           <div class="input-group-append">
-            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="collapse" data-target="#collapseFilter" aria-expanded="false" aria-controls="collapseFilter">
               Filter
             </button>
-            <div class="dropdown-menu p-4">
-              <strong>IMPLEMENT FILTERING</strong>
+          </div>
+        </div>
+
+        <div class="collapse" id="collapseFilter">
+          <div class="row align-items-center justify-content-around">
+            <div class="form-row col-12">
+              <!-- User ID -->
+              <div class="form-group col-md-2">
+                <label for="inputUserID">User ID</label>
+                <input type="number" min=1 class="form-control" id="inputUserID">
+              </div>
+              <!-- Email -->
+              <div class="form-group col-md-3">
+                <label for="inputUserEmail">Email</label>
+                <input type="text" class="form-control" id="inputUserEmail" placeholder="Item name">
+              </div>
+              <!-- Phone Number -->
+              <div class="form-group col-md-3">
+                <label for="inputUserPhone">Phone Number</label>
+                <input type="text" class="form-control" id="inputUserPhone" placeholder="Item name">
+              </div>
+              <!-- Min Date -->
+              <div class="form-group col-md-2">
+                <label for="inputMinDate">Minimum Date</label>
+                <input type="date" class="form-control" id="inputMinDate">
+              </div>
+              <!-- Max Date -->
+              <div class="form-group col-md-2">
+                <label for="inputMaxDate">Maximum Date</label>
+                <input type="date" class="form-control" id="inputMaxDate">
+              </div>
             </div>
           </div>
         </div>
