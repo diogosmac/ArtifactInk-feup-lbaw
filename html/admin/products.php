ea3dc9ea3fdb577b4c5429b5b2f98f33f6fc3ed0
Marantesss <up201706917@fe.up.pt>
2020-03-15 17:54:10 +0000

    Finished double handler slider

--- html/admin/products.php
+++ html/admin/products.php
@@ -34,43 +34,53 @@ draw_navbar();
         </div>
 
         <div class="collapse" id="collapseFilter">
-          <div>
-            <label class="mt-2" for="categories">Categories</label>
-            <div id="categories" class="rounded border border-secondary p-2" style="max-height:10em; overflow-y: scroll">
-              <?php for ($i = 1; $i < 6; $i++) { ?>
+          <div class="row align-items-center justify-content-around">
+            <div class="col-md-6 col-sm-12">
+              <label for="categories">Categories</label>
+              <div id="categories" class="rounded border p-2 search-box-category">
+                <?php for ($i = 1; $i < 6; $i++) { ?>
 
-                <div class="custom-control custom-checkbox mb-3">
-                  <input type="checkbox" class="custom-control-input" id="category<?= $i ?>">
-                  <label class="custom-control-label" for="category<?= $i ?>">Category <?= $i ?></label>
-                </div>
-              <?php } ?>
+                  <div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="category<?= $i ?>">
+                    <label class="custom-control-label" for="category<?= $i ?>">Category <?= $i ?></label>
+                  </div>
+                <?php } ?>
+              </div>
             </div>
-          </div>
-          <div>
-            <label class="mt-3" for="brands">Brands</label>
-            <div id="brands" class="rounded border border-secondary p-2" style="max-height:10em; overflow-y: scroll">
-              <?php for ($i = 1; $i < 6; $i++) { ?>
+            <div class="col-md-6 col-sm-12">
+              <label for="brands">Brands</label>
+              <div id="brands" class="rounded border p-2 search-box-category">
+                <?php for ($i = 1; $i < 6; $i++) { ?>
 
-                <div class="custom-control custom-checkbox mb-3">
-                  <input type="checkbox" class="custom-control-input" id="brand<?= $i ?>">
-                  <label class="custom-control-label" for="brand<?= $i ?>">Brand <?= $i ?></label>
-                </div>
-              <?php } ?>
+                  <div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="brand<?= $i ?>">
+                    <label class="custom-control-label" for="brand<?= $i ?>">Brand <?= $i ?></label>
+                  </div>
+                <?php } ?>
+              </div>
             </div>
+
+            <div class="col-2 text-center">
+              <div class="custom-control custom-switch my-3">
+                <input type="checkbox" class="custom-control-input" id="stockSwitch">
+                <label class="custom-control-label" for="stockSwitch">In-Stock</label>
+              </div>
+            </div>
+
+            <div class="col-10">
+              <div class="range-slider mb-2">
+                <label for="price">Price:
+                  <span class="rangeValues"></span>
+                </label>
+                <input type="range" class="custom-range price-slider" name="minprice" value="0" min="0" max="200" step="1">
+                <input type="range" class="custom-range price-slider" name="maxprice" value="200" min="0" max="200" step="1">
+              </div>
+            </div>
+
           </div>
-          <div class="custom-control custom-switch my-3">
-            <input type="checkbox" class="custom-control-input" id="stockSwitch">
-            <label class="custom-control-label" for="stockSwitch">In-Stock</label>
-          </div>
-          <div>
-            <label for="minPrice">Minimum Price</label>
-            <input type="range" class="custom-range" value="20" min="0" max="500" step="2" id="minPrice">
-          </div>
-          <div>
-            <label for="maxPrice">Maximum Price</label>
-            <input type="range" class="custom-range" value="100" min="0" max="500" step="2" id="maxPrice">
-          </div>
+
         </div>
+
 
         <table class="table table-striped text-center">
           <thead>
