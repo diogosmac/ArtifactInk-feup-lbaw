02423bfd33fb1b0e763791542f29a77c9a3c1215
Marantesss <up201706917@fe.up.pt>
2020-03-19 18:00:35 +0000

    Finished user page

--- html/templates/tpl_admin.php
+++ html/templates/tpl_admin.php
@@ -389,7 +389,7 @@ function draw_order_row($order)
 <?php }
 
 /**
- * Function to draw category row
+ * Function to draw review row
  */
 function draw_review_row($review)
 { ?>
@@ -466,6 +466,65 @@ function draw_review_row($review)
             </div>
             <div class="modal-body">
               <p class="text-justify"><?= $review['body'] ?></p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-link a_link" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </td>
+    <td class="align-middle">
+      <button type="button" class="btn btn-link a_link">
+        Delete
+      </button>
+    </td>
+  </tr>
+<?php }
+
+/**
+ * Function to user row
+ */
+function draw_user_row($user)
+{ ?>
+  <tr>
+    <th class="align-middle" scope="row"><?= $user['id'] ?></th>
+    <td class="align-middle"><?= $user['firstName'] ?></td>
+    <td class="align-middle"><?= $user['lastName'] ?></td>
+    <td class="align-middle"><?= $user['email'] ?>/td>
+    <td class="align-middle"><?= $user['phone'] ?></td>
+    <td class="align-middle"><?= $user['birthday'] ?></td>
+    <td class="align-middle">
+      <button type="button" class="btn button-secondary" data-toggle="modal" data-target="#viewUser<?= $user['id'] ?>">
+        View Billing
+      </button>
+      <!-- Modal -->
+      <div class="modal fade" id="viewUser<?= $user['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="viewUser<?= $user['id'] ?>Modal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="viewUser<?= $user['id'] ?>Modal">Billing Information</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <table class="table table-striped text-center">
+                <thead>
+                  <tr>
+                    <th scope="col">Credit Card</th>
+                    <th scope="col">Address 1</th>
+                    <th scope="col">Address 2</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="align-middle"><?= $user['card'] ?></td>
+                    <td class="align-middle"><?= $user['address1'] ?></td>
+                    <td class="align-middle"><?= $user['address2'] ?></td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-link a_link" data-dismiss="modal">Close</button>
