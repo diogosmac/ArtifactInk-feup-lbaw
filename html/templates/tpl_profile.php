ed520fac5452fe4581fba89fd0e1d7305067f648
Marantesss <up201706917@fe.up.pt>
2020-03-22 22:58:32 +0000

    Cleaned css + replaced material icons

--- html/templates/tpl_profile.php
+++ html/templates/tpl_profile.php
@@ -2,7 +2,7 @@
 { ?>
 
   <section id="profile-info">
-    <div class="col px-0">
+    <div class="col">
 
       <div class="row" id="profile-tag">
         <span>Photo</span>
@@ -87,66 +87,64 @@
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fi%2F301761272642-0-1%2Fs-l1000.jpg&f=1&nofb=1" alt="Ink" class="img-fluid">
       </div>
       <div class="col" id="product-review-info">
-        <div class="row my-1 align-items-center">
-          <div class="col" id="review-text">
-            <span class="review-product-name float-left">Black Ink</span>
-          </div>
-          <div class="col-md-auto" id="profile-edit-review">
-            <button type="button" class="btn float-right" id="profile-edit-review-button" data-toggle="modal" data-target="#exampleModalCenter">
-              <i class="material-icons">edit</i>
-            </button>
 
-            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Review on Black Ink</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <form>
-                      <div class="form-group">
-                        <label for="exampleFormControlInput1">Review Title</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" value="Cor viva, pouco resistente." placeholder="Write question here...">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Review</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
-                      </div>
-                    </form>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-link a_link" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn button">Save changes</button>
-                  </div>
+        <div class="d-flex justify-content-between my-1 align-items-center">
+          <span class="review-product-name float-left">Black Ink</span>
+
+          <button type="button" class="btn button-secondary float-right" id="profile-edit-review-button" data-toggle="modal" data-target="#exampleModalCenter">
+            <i class="fas fa-pen pr-2"></i>Edit
+          </button>
+
+          <!-- MODAL -->
+          <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle">Review on Black Ink</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form>
+                    <div class="form-group">
+                      <label for="exampleFormControlInput1">Review Title</label>
+                      <input type="text" class="form-control" id="exampleFormControlInput1" value="Cor viva, pouco resistente." placeholder="Write question here...">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleFormControlTextarea1">Review</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-link a_link" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn button">Save changes</button>
                 </div>
               </div>
             </div>
           </div>
+
         </div>
-        <div class="row my-1 align-items-center">
-          <div class="col" id="review-text">
-            <span class="review-title">Cor viva, pouco resistente.</span>
-          </div>
-          <div class="col-md-auto" id="profile-review-rating">
-            <div class="row float-right">
-              <i class="material-icons" style="color: gold;">star</i>
-              <i class="material-icons" style="color: gold;">star</i>
-              <i class="material-icons" style="color: gold;">star</i>
-              <i class="material-icons" style="color: gold;">star_half</i>
-              <i class="material-icons" style="color: gold;">star_outline</i>
-            </div>
+
+        <div class="d-flex justify-content-between my-1 align-items-center">
+          <span class="review-title">Cor viva, pouco resistente.</span>
+          <div>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+            <i class="far fa-star"></i>
           </div>
         </div>
-        <div class="row my-1">
+
+        <p class="my-1 text-justify">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
           ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
           fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
           deserunt mollit anim id est laborum.
-        </div>
+        </p>
       </div>
     </div>
   </section>
@@ -166,17 +164,19 @@
         </div>
       </div>
       <div class="col-md-auto float-right" id="wishlist-product-remove">
-        <button type="button" class="btn float-right btn-outline-danger"><i class="fas fa-trash"></i></button>
+        <button type="button" class="btn button-secondary float-right">
+          <i class="fas fa-trash pr-2"></i>Remove
+        </button>
       </div>
     </div>
     <div class="d-flex flex-row justify-content-between my-1">
       <div class="col-md-auto" id="wishlist-product-details">
         <div class="d-flex flex-row" id="wishlist-product-rating">
-          <i class="material-icons" style="color: gold;">star</i>
-          <i class="material-icons" style="color: gold;">star</i>
-          <i class="material-icons" style="color: gold;">star</i>
-          <i class="material-icons" style="color: gold;">star_half</i>
-          <i class="material-icons" style="color: gold;">star_outline</i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+          <i class="far fa-star"></i>
         </div>
         <div class="d-flex flex-row align-items-center">
           <span id="wishlist-product-available">Available&nbsp;</span>
