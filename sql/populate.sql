10eee0dec480d6d7d5c5488d83ad1c0942d35923
Marantesss <up201706917@fe.up.pt>
2020-03-24 10:39:12 +0000

    Added populate script skeleton

---
+++ sql/populate.sql
@@ -0,0 +1,65 @@
+-- user
+
+-- country
+
+-- city
+
+-- address
+
+-- credit_card
+
+-- paypal
+
+-- payment_method
+
+-- order
+
+-- category
+
+-- subcategory
+
+-- item
+
+-- archived_item
+
+-- item_purchase
+
+-- review
+
+-- cart
+
+-- wishlist
+
+-- picture
+
+-- item_picture
+
+-- profile_picture
+
+-- admin_notification
+
+-- out_of_stock_notification
+
+-- report_notification
+
+-- sale
+
+-- fixed_sale
+
+-- percentage_sale
+
+-- item_subscriber
+
+-- user_address
+
+-- user_payment_method
+
+-- support_chat_message
+
+-- newsletter_subscriber
+
+-- admin
+
+-- faq
+
+-- store
