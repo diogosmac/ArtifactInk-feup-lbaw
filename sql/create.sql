4e77d7327bb2324fc9593be131202bf52dd20859
Tiago Silva <tiago.silva.99@hotmail.com>
2020-04-10 21:56:54 +0100

    Updated create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -37,6 +37,15 @@ DROP TYPE IF EXISTS SALE_TYPE;
 DROP TYPE IF EXISTS ORDER_STATUS;
 DROP TYPE IF EXISTS MESSAGE_SENDER;
 
+-- Drop Triggers
+
+DROP TRIGGER IF EXISTS update_rating on "review";
+DROP TRIGGER IF EXISTS add_to_cart on "cart";
+DROP TRIGGER IF EXISTS new_oos_notification on "out_of_stock_notification";
+DROP TRIGGER IF EXISTS new_report_notification on "report_notification";
+DROP TRIGGER IF EXISTS new_item_sale on "item_sale";
+
+
 -- Types
 
 CREATE TYPE ITEM_STATUS AS ENUM ('active', 'archived');
@@ -120,7 +129,7 @@ CREATE TABLE "item" (
     "stock" INTEGER NOT NULL CONSTRAINT item_stock_ck CHECK ("stock" >= 0),
     "description" TEXT,
     "rating" FLOAT CONSTRAINT item_rating_ck CHECK (
-        ("rating" is NULL) or ("rating" is NOT NULL and "rating" > 0 and "rating" <= 0)
+        ("rating" is NULL) or ("rating" is NOT NULL and "rating" > 0 and "rating" <= 5)
     ),
     "status" ITEM_STATUS NOT NULL DEFAULT 'active',
     "id_category" INTEGER REFERENCES "category" ("id") ON UPDATE CASCADE
@@ -258,32 +267,36 @@ CREATE TABLE "store" (
 
 -- Indexes
 
-CREATE INDEX email_user ON "user" USING hash (email); 
-CREATE INDEX id_item ON item USING hash (id); 
-CREATE INDEX id_category ON category USING hash (id); 
-CREATE INDEX id_user_cart ON cart USING hash (id_user); 
+CREATE INDEX idx_user_picture ON "user" USING btree(id_picture);  
+CREATE INDEX idx_review ON "review" USING btree(id_user);
+CREATE INDEX idx_item_picture ON item_picture USING hash (id_item); 
 CREATE INDEX search_idx ON item USING GIST (to_tsvector('english',"item"."name" || ' ' || "item"."brand"|| ' ' || "item"."description"))
+
 
 -- Triggers
 
-CREATE OR REPLACE FUNCTION upd_rating()
+-- TRIGGER01 --
+CREATE OR REPLACE FUNCTION update_rating()
   RETURNS trigger AS
 $$
 BEGIN
         UPDATE "item" 
-        SET rating = (SELECT AVG(score) FROM review WHERE id = NEW.id_item)
+        SET rating = (SELECT AVG(score) FROM review WHERE id_item = NEW.id_item)
         WHERE id = NEW.id_item;
     RETURN NEW;
 END;
 $$
 LANGUAGE 'plpgsql';
---
+
 CREATE TRIGGER update_rating
   AFTER INSERT ON "review"
   FOR EACH ROW
-  EXECUTE PROCEDURE upd_rating();
+  EXECUTE PROCEDURE update_rating();
 
-  CREATE OR REPLACE FUNCTION add_to_cart()
+
+-- TRIGGER02 --
+
+CREATE OR REPLACE FUNCTION add_to_cart()
   RETURNS trigger AS
 $$
 BEGIN
@@ -301,58 +314,141 @@ CREATE TRIGGER add_to_cart
   ON "cart"
   FOR EACH ROW
   EXECUTE PROCEDURE add_to_cart();
---
-CREATE OR REPLACE FUNCTION check_stock()
-  RETURNS trigger AS
-$$
-DECLARE
-stock int;
-BEGIN
-    stock = (SELECT stock FROM "item" WHERE item=NEW.id_item)
-    IF (stock < NEW.quantity)
-    THEN 
-      RAISE EXCEPTION 'Not enough stock';
-    END IF;
-    RETURN NEW;
-END;
-$$
-LANGUAGE 'plpgsql';
 
-CREATE TRIGGER check_stock
-  BEFORE INSERT ON "item_purcahse"
-  FOR EACH ROW
-  EXECUTE PROCEDURE check_stock();
---
-CREATE OR REPLACE FUNCTION update_stock()
+
+-- TRIGGER03 --
+
+CREATE OR REPLACE FUNCTION new_oos_notification()
   RETURNS trigger AS
 $$
-DECLARE
-new_stock int;
-old_stock int;
-notif_id int;
-user int;
 BEGIN
-    old_stock = (SELECT stock FROM item WHERE id = NEW."id_item");
-    new_stock = old_stock - NEW.quantity;
-    IF (new_stock = 0) 
+    IF EXISTS (SELECT id_notif FROM "report_notification" WHERE id_notif=NEW.id_notif) 
 	  THEN 
-		  INSERT INTO "admin_notification" ("body") VALUES (DEFAULT, 'Product out of stock') RETURNING id INTO notif_id;	
-		  INSERT INTO "out_of_stock_notification"("id_notif", "id_item") VALUES (notif_id, NEW.id_item);
+		  RAISE EXCEPTION 'Notification is already of type out_of_stock_notification' ;
     END IF;
-
-    UPDATE "item" SET stock = new_stock
-    WHERE id = id_item;
-
-    user = (SELECT id_user FROM "order" WHERE id=NEW.id_order)
-    DELETE FROM "cart" WHERE id_user=user AND id_item=NEW.id_item;
-
   RETURN NEW;
 END;
 $$
 LANGUAGE 'plpgsql';
 
-CREATE TRIGGER update_stock
-  AFTER INSERT
-  ON "item_purchase"
+
+CREATE TRIGGER new_oos_notification
+  BEFORE INSERT
+  ON "out_of_stock_notification"
   FOR EACH ROW
-  EXECUTE PROCEDURE update_stock();
+  EXECUTE PROCEDURE new_oos_notification();
+
+
+-- TRIGGER04 --
+
+CREATE OR REPLACE FUNCTION new_report_notification()
+  RETURNS trigger AS
+$$
+BEGIN
+    IF EXISTS (SELECT id_notif FROM "out_of_stock_notification" WHERE id_notif=NEW.id_notif) 
+	  THEN 
+		  RAISE EXCEPTION 'Notification is already of type report_notification' ;
+    END IF;
+  RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER new_report_notification
+  BEFORE INSERT
+  ON "report_notification"
+  FOR EACH ROW
+  EXECUTE PROCEDURE new_report_notification();
+
+
+  -- TRIGGER05 --
+
+  CREATE OR REPLACE FUNCTION new_item_sale()
+  RETURNS trigger AS
+$$
+DECLARE 
+    item_on_sale RECORD;
+    sale_fixed_amount float;
+    sale_start date;
+    sale_end date;
+
+BEGIN
+    SELECT fixed_amount, "start", "end" INTO sale_fixed_amount, sale_start, sale_end FROM sale WHERE id=NEW.id_sale;
+    IF sale_fixed_amount IS NOT NULL THEN
+        IF (SELECT price FROM item WHERE id=NEW.id_item) < sale_fixed_amount THEN
+            RAISE EXCEPTION 'Sale amount is higher than the price of the item';
+        END IF; 
+    END IF;
+
+    IF EXISTS (SELECT * FROM "item", "sale", "item_sale" WHERE item.id=item_sale.id_item AND sale.id=item_sale.id_sale AND sale.start <= sale_end AND sale.end >= sale_start) 
+	  THEN 
+		  RAISE EXCEPTION 'The sale period colides with another sale for this item';
+    END IF;
+  RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER new_item_sale
+  BEFORE INSERT
+  ON "item_sale"
+  FOR EACH ROW
+  EXECUTE PROCEDURE new_item_sale();
+
+
+
+-- Transaction Functions 
+
+CREATE OR REPLACE FUNCTION purchase_items(user_id int) RETURNS void AS $$
+DECLARE
+    purchase_product RECORD;
+    product_price float;
+BEGIN
+
+    FOR purchase_product IN SELECT * FROM "cart" WHERE id_user=user_id LOOP
+        SELECT * INTO item_sale_rec FROM item, sale, item_sale WHERE item.id=purchase_product.id_item AND item.id=item_sale.id_item AND sale.id=item_sale.id_sale AND sale.end > CURRENT_DATE;
+        
+        IF NOT FOUND THEN
+            product_price = (SELECT price FROM "item" WHERE id=purchase_product.id_item);         
+
+        ELSE -- CALCULATE POSSIBLE SALE 
+            
+            IF item_sale_rec.fixed_amount IS NOT NULL THEN
+                product_price = item_sale_rec.price - item_sale_rec.fixed_amount;
+            ELSE 
+                product_price = item_sale_rec.price * (100 - item_sale_rec.percentage_amount) / 100;
+            END IF; 
+        
+        END IF;
+        
+        INSERT INTO "item_purchase" ("id_item", "id_order", "price", "quantity") VALUES (purchase_product.id_item, currval('order_id_seq'), product_price, purchase_product.quantity);
+    
+    END LOOP;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION checkout_transaction(user_id int, address_id int, payment_method_id int) RETURNS void AS $$
+DECLARE
+    product_in_cart RECORD;
+    order_total float;
+BEGIN
+	
+	IF EXISTS (SELECT * FROM "item", "cart" WHERE item.id=cart.id_item AND cart.id_user=user_id AND item.stock < cart.quantity)
+		THEN RAISE EXCEPTION 'Not enough stock';
+	ELSE 
+		FOR product_in_cart IN SELECT id_item, quantity FROM "cart" WHERE id_user=user_id LOOP
+			UPDATE "item" SET stock = stock - product_in_cart.quantity WHERE id=product_in_cart.id_item;	
+		END LOOP;
+		
+        INSERT INTO "order" ("total", "id_user", "id_address", "id_payment_method") VALUES
+                            (1, user_id, address_id, payment_method_id);
+    	PERFORM purchase_items(user_id);
+
+        order_total = (SELECT SUM(price) FROM "item_purchase" WHERE id_order=currval('order_id_seq'));
+        UPDATE order SET total=order_total WHERE id=currval('order_id_seq');
+
+        DELETE FROM "cart" WHERE id_user = user_id;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
