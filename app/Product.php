072f2b36c85254250173f9c6c795a66de2b9a18a
Marantesss <up201706917@fe.up.pt>
2020-04-19 20:05:15 +0100

    Started product and categories model

---
+++ app/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'product';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * 
+     */
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+}
