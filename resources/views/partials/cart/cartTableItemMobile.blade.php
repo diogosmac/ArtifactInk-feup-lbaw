1284a75c4a0bd0b731c2d39113ca57f2c20724f7
Tiago Silva <tiago.silva.99@hotmail.com>
2020-04-20 01:26:56 +0100

    View cart page (large imgs on mobile)

--- resources/views/partials/cart/cartTableItemMobile.blade.php
+++ resources/views/partials/cart/cartTableItemMobile.blade.php
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-4 col-sm-3">
                 <a href="../pages/product.php" class="list-img-link" >
-                    <img src="https://amotatuagem.com/wp-content/uploads/2018/06/Unalome-Tattoo-10.jpg"
+                    <img src="{{ asset('storage/img_product/' . $picture->link) }}"
                         class="card-img-top" alt="...">
                 </a>
             </div>
             <div class="col-6 col-sm-6">
                 <a href="../pages/product.php" class="list-img-link">
-                    <h5 class="font-weight-bold"> Girly Design </h5>
+                    <h5 class="font-weight-bold"> {{ $item->name }} </h5>
                 </a>
              
                 <div class="py-1 d-flex flex-row bd-highlight justify-content-between-mobile">
@@ -17,7 +17,7 @@
                         <button type="button" class="btn btn-link a_link sub-button">
                             <i class="fas fa-minus"></i>
                         </button>
-                        <span class="item-quant"> 1</span>
+                        <span class="item-quant"> {{ $item->pivot->quantity }}</span>
                         <button type="button" class="btn btn-link a_link add-button">
                             <i class="fas fa-plus"></i>
                         </button>
@@ -28,8 +28,8 @@
 
             </div>
             <div class="py-2  col-2 col-sm-3 col-smd-flex flex-column justify-content-between align-items-end li-price">
-                <h5 class="font-weight-bold item-value" style="color: var(--main-red)"> 89.90€</h5>
-                <h6 class="font-weight-bold item-price  " style="color: var(--main-red)">89.90€</h>
+                <h5 class="font-weight-bold item-value" style="color: var(--main-red)">{{ $item->price }}</h5>
+                <h6 class="font-weight-bold item-price" style="color: var(--main-red)">{{ $item->price }}</h6>
                 <div>
                    <button type="button" class="btn btn-link a_link rmv-button">
                         <i class="fas fa-trash-alt"></i>
