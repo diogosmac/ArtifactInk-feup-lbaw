070942c6235aa471e15f02852fb8d97b61ea8333
Marantesss <up201706917@fe.up.pt>
2020-05-05 17:09:28 +0100

    Removed var dump in admin middleware!

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -11,13 +11,10 @@ class AdminMiddleware
 
     public function handle($request, Closure $next)
     {
-        dd(Auth::guard('admin'));
         // redirect to admin login page if admin not logged in
         if (!Auth::guard('admin')->check()) {
             return redirect()->route('admin.sign_in');
-        } else {
-            return $next($request);
-        }
+        } 
         
         return $next($request);
     }
