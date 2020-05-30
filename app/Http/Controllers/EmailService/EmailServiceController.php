b6da507dc8c17994179c78471dbd5ea9eef472da
Tiago Silva <tiago.silva.99@hotmail.com>
2020-05-30 20:12:06 +0100

    Sending reset pw email

--- app/Http/Controllers/EmailService/EmailServiceController.php
+++ app/Http/Controllers/EmailService/EmailServiceController.php
@@ -1,9 +1,12 @@
 <?php
-//require_once('./vendor/autoload.php');
 
 namespace App\Http\Controllers\EmailService;
 
+//require("vendor/autoload.php");
+
 use App\Http\Controllers\Controller;
+use Illuminate\Filesystem\Filesystem;
+use Exception;
 
 class EmailServiceController extends Controller
 {
@@ -11,29 +14,28 @@ class EmailServiceController extends Controller
 
     public function sendRecoverPasswordEmail($email, $name, $url)
     {
-        if (!isset($email) || !isset($name) || !isset($link)) {
+        if (!isset($email) || !isset($name) || !isset($url)) {
             return false;
         }
-        return;
         // Configure API key authorization: api-key
-        $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
+        $config = \SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
 
         // Uncomment below line to configure authorization using: partner-key
         // $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('partner-key', 'YOUR_API_KEY');
 
-        $apiInstance = new SendinBlue\Client\Api\SMTPApi(
+        $apiInstance = new \SendinBlue\Client\Api\SMTPApi(
             // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
             // This is optional, `GuzzleHttp\Client` will be used as default.
-            new GuzzleHttp\Client(),
+            new \GuzzleHttp\Client(),
             $config
         );
         $sendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail(); // \SendinBlue\Client\Model\SendSmtpEmail | Values to send a transactional email
         $sendSmtpEmail['sender'] = array('email' => 'general@artifactink.pt', 'name' => 'Artifact Ink');
-        $sendSmtpEmail['to'] = array(array('email' => $email, 'name' => $name));
+        $sendSmtpEmail['to'] = array(array('email' => 'up201705985@g.uporto.pt', 'name' => $name));
         $sendSmtpEmail['subject'] = 'Reset Password Request';
-
-        $htmlfile = file_get_contents('reset_password.html');
-        $htmlContent = sprintf($htmlfile, 'User', $url, $url);
+        
+        $content = $this->htmlEmail();
+        $htmlContent = sprintf($content, $name, $url, $url);
 
         $sendSmtpEmail['htmlContent'] = $htmlContent;
         $sendSmtpEmail['textContent'] = 'SENDINBLUE AUTO EMAIL';
@@ -43,7 +45,7 @@ class EmailServiceController extends Controller
         try {
             $apiInstance->sendTransacEmail($sendSmtpEmail);
         } catch (Exception $e) {
-            //echo 'Exception when calling SMTPApi->sendTransacEmail: ', $e->getMessage(), PHP_EOL;
+            echo 'Exception when calling SMTPApi->sendTransacEmail: ', $e->getMessage(), PHP_EOL;
             return false;
         }
 
@@ -52,5 +54,124 @@ class EmailServiceController extends Controller
 
     public function sendNewsletterEmail($email) {
     
+    }
+
+    public function htmlEmail()
+    {
+        return "<html>
+
+        <head>
+            <title>Artifact Ink</title>
+        
+            <style>
+                body {
+                    background-color: #eeeeee;
+                    text-align: center;
+                }
+        
+                section.main {
+                    margin: auto;
+                    margin-top: 1em;
+                    max-width: 800px;
+                    background-color: white;
+                    padding: 2em 0em;
+                }
+        
+                article.content {
+                    margin: auto;
+                    padding: 0em 1em;
+                    font-size: 18px;
+                    color: #555555;
+                    text-align: start;
+                }
+        
+                article.content .highlight {
+                    color: black;
+                }
+        
+                div.button-container {
+                    padding: 1em; text-align: center;
+                }
+        
+                div.button-container a {
+                    color: #F1F1F2;
+                }
+        
+                .button {
+                    text-decoration: none;
+                    cursor: pointer;
+                    border-radius: 0;
+                    background-color: #8C4748;
+                    color: #F1F1F2;
+                    border-color: #8C4748;
+                }
+        
+                .button:hover {
+                    background-color: #731F20;
+                    color: #F1F1F2;
+                    border-color: #731F20;
+                }
+        
+                .button:active,
+                .button:focus {
+                    background-color: #8C4748;
+                    color:  #F1F1F2;
+                    border-color: #8C4748;
+                    box-shadow: 0 0 0;
+                }
+        
+                .btn {
+                    padding: .5rem 1rem;
+                    font-size: 1.25rem;
+                    line-height: 1.5;
+                    border-radius: .25rem;
+        
+                    display: inline-block;
+                    font-weight: 400;
+                    text-align: center;
+                    white-space: nowrap;
+                    vertical-align: middle;
+                    -webkit-user-select: none;
+                    -moz-user-select: none;
+                    -ms-user-select: none;
+                    user-select: none;
+                    border: 1px solid transparent;
+                    line-height: 1.5;
+                    transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+                }
+        
+                p.trouble {
+                    text-align:center; 
+                    color: #555555; 
+                    margin-top: 3em; 
+                    font-size: 14px;
+                }
+            </style>
+        </head>
+        
+        <body style=\"font-family: Cambria, Cochin, Georgia, Times, serif;\">
+            <div>
+            <nav>
+                <img src=\"https://web.fe.up.pt/~up201705985/images/artifact_ink_logo_letters.png\" alt=\"Logo\" width=\"400\" style=\"display: block;margin-left: auto;margin-right: auto;\">
+            </nav>
+        
+            <section class=\"main\">
+                <article class=\"content\">
+                    <h3 class=\"highlight\">Hi %s,</h3>
+                    <p>You recently requested to reset your password for your Artifact Ink account.</p>
+                    <p>Click the button bellow to reset it.</p>
+                    <div class=\"button-container\">
+                        <a class=\"btn button\" href=\"%s\">Reset Password</a>
+                    </div>
+                    <p>If you did not request a password reset, please ignore this email. This password reset is only valid for
+                        the next 30 minutes.</p>
+                    <p>Thanks,<br>The Artifact Ink Team</p>
+                </article>
+        
+                <p class=\"trouble\">If you're having trouble clicking the button, please follow this link <br>%s</p>
+            </section>
+        </div>
+        </body>
+        </html>";
     }
 }
