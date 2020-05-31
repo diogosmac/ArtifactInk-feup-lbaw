999854a70d6b9a5bb68a52c60a7c2920f626bf97
Gustavo nRm <gustavonrm@gmail.com>
2020-05-31 19:41:39 +0100

    paypal api - start

---
+++ second.php
@@ -0,0 +1,57 @@
+<?php
+
+// 1. Autoload the SDK Package. This will include all the files and classes to your autoloader
+require __DIR__  . '/vendor/autoload.php';
+
+// 2. Provide your Secret Key. Replace the given one with your app clientId, and Secret
+// https://developer.paypal.com/webapps/developer/applications/myapps
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'AU2wfS44EU_WdD-1A8Tco3L5WCxhYY9ZClgu6g2QvGXqmFsSbZm-Hsdu3Jm8pbrP8TXXm9FwBk8zUdK-',     // ClientID
+        'ELVznGmVllI3lYXzDWb7R-N19PyOA44KzbvbveWSkfKv2jUOgn3dXGDrk-VAYEncumrXSlo7t6_OH_oZ'      // ClientSecret
+    )
+);
+
+// Step 2.1 : Between Step 2 and Step 3
+$apiContext->setConfig(
+    array(
+      'log.LogEnabled' => true,
+      'log.FileName' => 'PayPal.log',
+      'log.LogLevel' => 'DEBUG'
+    )
+);
+
+// 3. Lets try to create a Payment
+// https://developer.paypal.com/docs/api/payments/#payment_create
+$payer = new \PayPal\Api\Payer();
+$payer->setPaymentMethod('paypal');
+
+$amount = new \PayPal\Api\Amount();
+$amount->setTotal('1.00');
+$amount->setCurrency('EUR');
+
+$transaction = new \PayPal\Api\Transaction();
+$transaction->setAmount($amount);
+
+$redirectUrls = new \PayPal\Api\RedirectUrls();
+$redirectUrls->setReturnUrl("https://example.com/your_redirect_url.html")
+    ->setCancelUrl("https://example.com/your_cancel_url.html");
+
+$payment = new \PayPal\Api\Payment();
+$payment->setIntent('sale')
+    ->setPayer($payer)
+    ->setTransactions(array($transaction))
+    ->setRedirectUrls($redirectUrls);
+
+// 4. Make a Create Call and print the values
+try {
+    $payment->create($apiContext);
+    echo $payment;
+
+    echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
+}
+catch (\PayPal\Exception\PayPalConnectionException $ex) {
+    // This will print the detailed information on the exception.
+    //REALLY HELPFUL FOR DEBUGGING
+    echo $ex->getData();
+}
