bd86e636a9b390d305e7d05a0ca3299a02d7ccfb
Marantesss <up201706917@fe.up.pt>
2020-06-01 17:44:03 +0100

    Started sales page

--- resources/views/partials/admin/sales_row.blade.php
+++ resources/views/partials/admin/sales_row.blade.php
@@ -1,8 +1,14 @@
 <tr>
   <th class="align-middle" scope="row">{{ $sale->id }}</th>
   <td class="align-middle">{{ $sale->name }}</td>
-  <td class="align-middle">{{ $sale->startTimestamp }}</td>
-  <td class="align-middle">{{ $sale->endTimestamp }}</td>
+  <td class="align-middle">{{ ucfirst($sale->type) }}</td>
+  @if(!is_null($sale->fixed_amount))
+  <td class="align-middle">{{ $sale->fixed_amount }}€</td>
+  @else
+  <td class="align-middle">{{ $sale->percentage_amount }}%</td>
+  @endif
+  <td class="align-middle">{{ $sale->start }}</td>
+  <td class="align-middle">{{ $sale->end }}</td>
   <td class="align-middle">
     <button type="button" class="btn button-secondary" onclick="location.href='{{ url('/admin/sales/' . $sale->id . '/edit') }}'">Edit</button>
     <button type="button" class="btn btn-link a_link">Remove</button>
