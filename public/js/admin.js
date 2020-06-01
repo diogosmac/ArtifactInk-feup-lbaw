bf2db270e369c8a66fe8ecb5e57da69d17b72325
Marantesss <up201706917@fe.up.pt>
2020-06-01 23:41:01 +0100

    Added js for item sale addition and remove

--- public/js/admin.js
+++ public/js/admin.js
@@ -166,14 +166,68 @@ function unbanUserHandler() {
  * SALE ITEMS
  * 
  */
+let addItemList;
+let itemList;
+let saleForm;
 function addSaleItemsForm() {
-    let submitButton = document.getElementById('submit-sale-button');
-    let saleForm = document.getElementById('edit-sale');
-    if (submitButton == null || saleForm == null)
+    saleForm = document.getElementById('edit-sale');
+    itemList = document.getElementById('item-list');
+    addItemList = document.getElementById('add-item-list');
+    if (saleForm == null)
         return;
     
+    let addItemRows = document.getElementsByClassName('sale-add-product-row');
+    let removeItemRows = document.getElementsByClassName('sale-remove-product-row');
+    for (let i = 0; i < addItemRows.length; i++) {
+        let addButton = addItemRows[i].getElementsByClassName('add-item-button')[0];
+        addButton.addEventListener('click', addSaleItem);
+    }
+
+    for (let i = 0; i < removeItemRows.length; i++) {
+        let removeButton = removeItemRows[i].getElementsByClassName('remove-item-button')[0];
+        removeButton.addEventListener('click', removeSaleItem);
+    }
 }
 
+function addSaleItem(event) {
+    // get product id and row
+    let id = this.getAttribute('product-id');
+    let row = document.querySelector('tr[product-id="' + id + '"]');
+    // swap rows
+    row.parentNode.removeChild(row);
+    itemList.insertBefore(row, itemList.firstChild);
+    // change button style
+    this.classList.remove('button-secondary');
+    this.classList.remove('add-item-button');
+    this.classList.add('btn-link');
+    this.classList.add('a_link');
+    this.classList.add('remove-item-button');
+    this.innerHTML = "Remove"
+    // swap event listeners
+    this.removeEventListener('click', addSaleItem);
+    this.addEventListener('click', removeSaleItem);
+    event.preventDefault();
+}
+
+function removeSaleItem() {
+    // get product id and row
+    let id = this.getAttribute('product-id');
+    let row = document.querySelector('tr[product-id="' + id + '"]');
+    // swap rows
+    row.parentNode.removeChild(row);
+    addItemList.insertBefore(row, addItemList.firstChild);
+    // change button style
+    this.classList.remove('btn-link');
+    this.classList.remove('a_link');
+    this.classList.remove('remove-item-button');
+    this.classList.add('button-secondary');
+    this.classList.add('add-item-button');
+    this.innerHTML = "Add"
+    // swap event listeners
+    this.removeEventListener('click', removeSaleItem);
+    this.addEventListener('click', addSaleItem);
+    event.preventDefault();
+}
 /**
  * 
  * GENERAL INFO
--- resources/views/pages/admin/sales/edit_sale.blade.php
+++ resources/views/pages/admin/sales/edit_sale.blade.php
@@ -14,21 +14,37 @@
     </div>
 
     <form action="{{ route('admin.sales.edit', ['id' => $sale->id]) }}" method="POST" id="edit-sale">
+      @csrf
+      @method('PUT')
       <div class="form-row">
-        <div class="form-group col-md-6">
+        <div class="form-group col-md-4">
           <label for="inputTitle">Title</label>
           <input required name='name' type="text" class="form-control" id="inputTitle" value="{{ $sale->name }}">
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
+          <label for="inputStartDate">Type</label>
+          <select required name="type" class="form-control" id="questionNumber">
+            <option value="fixed" {{ ($sale->type == 'fixed') ? 'selected' : '' }}>Fixed</option>
+            <option value="percentage" {{ ($sale->type == 'percentage') ? 'selected' : '' }}>Percentage</option>
+          </select>
+        </div>
+        <div class="form-group col-md-2">
+          <label for="inputStartDate">Value</label>
+          <input required name='value' type="number" class="form-control" id="inputStartDate" value="{{ ($sale->type == 'fixed') ? $sale->fixed_amount : $sale->percentage_amount }}">
+        </div>
+        <div class="form-group col-md-2">
           <label for="inputStartDate">Start Date</label>
           <input required name='start' type="date" class="form-control" id="inputStartDate" value="{{ $sale->start }}">
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
           <label for="inputEndDate">End Date</label>
           <input required name='end' type="date" class="form-control" id="inputEndDate" value="{{ $sale->end }}">
         </div>
       </div>
-      <input type="hidden" name='items[]' value="{{ $sale->items }}">
+      @foreach($sale->items->pluck('id') as $id_item)
+      <input id='item-{{ $id_item }}' type="hidden" name='items[{{ $loop->index }}]' value="{{ $id_item }}">
+      @endforeach
+      <input type="hidden" name='id' value="{{ $sale->id }}">
     </form>
 
     <div class="mx-auto mt-2">
@@ -102,7 +118,7 @@
                 <th scope="col"></th>
               </tr>
             </thead>
-            <tbody>
+            <tbody id='add-item-list'>
               @each('partials.admin.sale_add_product_row', $items, 'product')
             </tbody>
           </table>
@@ -177,7 +193,7 @@
                 <th scope="col"></th>
               </tr>
             </thead>
-            <tbody>
+            <tbody id='item-list'>
               @each('partials.admin.sale_remove_product_row', $items_sale, 'product')
             </tbody>
           </table>
