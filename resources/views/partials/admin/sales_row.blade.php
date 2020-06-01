1a1df0b0082e1cf35ff321aaf976306c0a5cdc29
Marantesss <up201706917@fe.up.pt>
2020-06-01 18:07:33 +0100

    Deleting sales is now something we can do :)

--- resources/views/partials/admin/sales_row.blade.php
+++ resources/views/partials/admin/sales_row.blade.php
@@ -9,8 +9,13 @@
   @endif
   <td class="align-middle">{{ $sale->start }}</td>
   <td class="align-middle">{{ $sale->end }}</td>
-  <td class="align-middle">
+  <td class="align-middle d-flex justify-content-center">
     <button type="button" class="btn button-secondary" onclick="location.href='{{ url('/admin/sales/' . $sale->id . '/edit') }}'">Edit</button>
-    <button type="button" class="btn btn-link a_link">Remove</button>
+    <form action="{{ route('admin.sales.delete') }}" method="POST">
+      @csrf
+      @method('DELETE')
+      <input type="hidden" name="id" value="{{ $sale->id }}">
+      <button type="submit" value="submit" class="btn btn-link a_link">Remove</button>
+    </form>
   </td>
 </tr>
