4015967ff6a51ea90f4b064cc64d04b8767f21c5
Diogo Machado <diogo.smac@gmail.com>
2020-06-02 03:46:05 +0100

    Added sales to cart (both in popup and in the cart page)

--- resources/views/partials/cart/cartListItem.blade.php
+++ resources/views/partials/cart/cartListItem.blade.php
@@ -5,7 +5,29 @@
         alt="{{ $cart_item->name }}">
     </span>
     <h5 class="cart-item-list-name"> {{ $cart_item->name }} </h5>
-    <h6 class="cart-item-list-price">{{ $cart_item->price }} </h6>
+    <?php
+        $sales = $cart_item->sales;
+        $currentSale = 0;
+        $price = $cart_item->price;
+        foreach ($sales as $sale) {
+            $new_sale = 0;
+            if ($sale->type == 'percentage') {
+                $new_sale = 0.01 * $sale->percentage_amount * $price;
+            } else if ($sale->type == 'fixed') {
+                $new_sale = $sale->fixed_amount;
+            }
+            if ($new_sale > $currentSale) {
+                $currentSale = $new_sale;
+            }
+        }
+        $price = round($price - $currentSale, 2);
+    ?>
+    <h6>
+        @if ($price != $cart_item->price)
+        <span class="old-price"> {{ $cart_item->price }} </span>
+        @endif
+        <span class="cart-item-list-price"> {{ $price }} </span>
+    </h6>
     <h6 class="badge badge-primary badge-pill cart-item-list-quant">{{ $cart_item->pivot->quantity }} </h6>
   </li>
 </a>
--- resources/views/partials/cart/cartTableItem.blade.php
+++ resources/views/partials/cart/cartTableItem.blade.php
@@ -9,7 +9,37 @@
             {{ $item->name }}
         </a>
     </th>
-    <td colspan="0" class="item-price">{{ $item->price }}</td>
+    <?php
+        $sales = $item->sales;
+        $currentSale = 0;
+        $price = $item->price;
+        foreach ($sales as $sale) {
+            $new_sale = 0;
+            if ($sale->type == 'percentage') {
+                $new_sale = 0.01 * $sale->percentage_amount * $price;
+            } else if ($sale->type == 'fixed') {
+                $new_sale = $sale->fixed_amount;
+            }
+            if ($new_sale > $currentSale) {
+                $currentSale = $new_sale;
+            }
+        }
+        $price = round($price - $currentSale, 2);
+    ?>
+    
+    <?php // PARA MOSTRAR O PREÇO ANTIGO ?>
+    @if ($price == $item->price)
+        <td colspan="0" class="item-price">{{ $item->price }}</td>
+    @else
+        <td colspan="0">
+            <span class="old-price"> {{ $item->price}} </span>
+            <span class="item-price"> {{ $price }} </span>
+        </td>
+    @endif
+    
+    <?php // PARA NÃO MOSTRAR O PREÇO ANTIGO ?>
+    {{-- <td colspan="0" class="item-price"> {{ $price }} </td> --}}
+
     <td colspan="0">
         <button type="button" class="btn btn-link a_link sub-button">
             <i class="fas fa-minus"></i>
