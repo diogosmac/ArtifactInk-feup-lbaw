d27428117547e2aa7e2ade87265d1865979f2f25
Marantesss <up201706917@fe.up.pt>
2020-06-03 14:41:11 +0100

    Removed search from sales

--- resources/views/pages/admin/reviews.blade.php
+++ resources/views/pages/admin/reviews.blade.php
@@ -6,7 +6,7 @@
 <main class="col-md-9 ml-sm-auto col-lg-10 px-4">
   <div class="container">
 
-    <div class="mb-4 mt-2">
+    <div class="mb-4 mt-2 border-bottom">
       <h1>Reviews</h1>
     </div>
 
